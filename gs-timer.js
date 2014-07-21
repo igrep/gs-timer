677a3a1ce42d2daa82e652cfec279e5f6288c911
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-07-21 17:18:32 +0900

    Oops! recover mis-edited file.

--- gs-timer.js
+++ gs-timer.js
@@ -1,44 +1,15 @@
-<style>
-#container {
-  text-align: center;
+function onOpen() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.addMenu('Time Tracking', [{ "name": 'Lauch Timer', "functionName": 'launchTimer_'}]);
 }
-</style>
-<div id="container">
-  <h1>Timer</h1>
-  <form>
-    <input type="button" value="Start" id="toggleTimerButton" />
-  </form>
-</div>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script>
-var currentTimeSheetEntry = {
-  "row": null,
-  "column": null,
-  "measuring": false,
-  "$el": $("#toggleTimerButton"),
-  "prepare": function(){
-    var this_ = this;
-    this_.$el.click(function(){ this_.toggleTimer() }); 
-  },
-  "toggleTimer": function(){
-    var this_ = this;
-    if(this_.measuring){
-      this_.$el.val("Start");
-      this_.measuring = false;
-    } else {
-      google.script.run.withSuccessHandler(
-        function(position) { this_.rememberPosition(position) }
-      ).beginTimeSheetEntry();
-      this_.$el.val("Stop");
-      this_.measuring = true;
-    }
-  },
-  "rememberPosition": function(position){
-    this.row = position[0];
-    this.column = position[1];
-    console.log("row: " + this.row);
-    console.log("column: " + this.column);
-  }
+
+function launchTimer_() {
+  var ui = SpreadsheetApp.getUi();
+  ui.showSidebar(HtmlService.createHtmlOutputFromFile('timer.html'));
 }
-currentTimeSheetEntry.prepare();
-</script>
+
+function beginTimeSheetEntry() {
+  var cell = SpreadsheetApp.getActiveSheet().getActiveCell();
+  cell.setValue(new Date());
+  return [cell.getRow(), cell.getColumn()];
+}
